332a375c3dde1421162c0c8ab620475d687321c0
arduano <leonid.shchurov@gmail.com>
2023-12-13 19:56:45 +1100

    Small fixes for day13

---
+++ 2023/day14/src/main.rs
@@ -0,0 +1,16 @@
+use shared::*;
+
+const INPUT: &str = day_input!();
+
+fn part1() {
+    println!("Part 1: {}", -1)
+}
+
+fn part2() {
+    println!("Part 2: {}", -1)
+}
+
+fn main() {
+    part1();
+    part2();
+}
